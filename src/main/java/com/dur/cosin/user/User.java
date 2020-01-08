cbd1353d816f666cc8513537fd1555c1f12da68e
lukexin <lukexin@zhanchengkeji.com>
2020-01-08 20:03:22 +0800

    404页面

---
+++ src/main/java/com/dur/cosin/user/User.java
@@ -0,0 +1,23 @@
+package com.dur.cosin.user;
+
+public class User {
+
+    private int uid;
+    private String nickname;
+
+    public int getUid(){
+        return uid;
+    }
+
+    public String getNickname(){
+        return nickname;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
